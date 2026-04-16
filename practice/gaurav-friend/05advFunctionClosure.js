c6a3feb42e365204174933b1d0d0386a873cff23
prince <princeguptacss@gmail.com>
2026-04-16 19:57:08 +0530

    functions practice done

--- practice/gaurav-friend/05advFunctionClosure.js
+++ practice/gaurav-friend/05advFunctionClosure.js
@@ -17,3 +17,55 @@ sayHi();
 // 🔥 Definition (simple)
 
 // 👉 Closure = function + its outer variables
+
+//🧠 Example:
+
+// function outer() {
+//   count = 0;
+
+//   function inner() {
+//     count += 1;
+//     console.log(count);
+//   }
+
+//   return inner;
+// }
+
+// const fn = outer();
+
+// fn();
+// fn();
+// fn();
+// fn();
+
+// Q1 c create a counter using closure⭐
+
+// function outer() {
+//   count = 0;
+
+//   function inner() {
+//     count++;
+//     console.log(count);
+//   }
+//   return inner;
+// }
+
+// const fn = outer();
+
+// fn();
+// fn();
+// fn();
+
+//Q2.
+
+function MultiplyBy(x) {
+  return function (y) {
+    return x * y;
+  };
+}
+
+let double = MultiplyBy(2);
+console.log(double(3));
+console.log(double(6));
+
+// heinn yeh kaise hogya
